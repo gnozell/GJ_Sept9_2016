dc1043f7744a4809547642e1552a85e138d11d9b
Jequatot <jeffreyquattro@gmail.com>
2016-09-09 21:10:20 -0400

    Rocket Dodging Game

---
+++ Assets/rocket_dodge.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rocket_dodge : MonoBehaviour {
+
+	public int timer;
+	public int spaceDown = -1;
+	public GameObject player;
+	public GameObject splosion;
+	public GameObject textSpace;
+	public GameObject text1;
+	public GameObject text2;
+	public GameObject text3;
+	public GameObject textGo;
+
+	Rigidbody2D rb;
+	BoxCollider2D bCollider;
+	int gamestate = 0;
+
+	// Use this for initialization
+	void Start () {
+		timer = 120;
+		Instantiate (textSpace, new Vector3 (0f, 2.27f, -1f), Quaternion.identity);
+		Instantiate (text3, new Vector3 (0f, -1.92f, -1f), Quaternion.identity);
+		player = this.gameObject;
+		rb = this.gameObject.GetComponent<Rigidbody2D>();
+		bCollider = this.gameObject.GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Gamestate 0 is intro
+		//Gamestate 1 is play
+		//Gamestate 2 is success
+		//Gamestate 3 is failure
+
+		if (gamestate == 0) {
+			if (timer > 0)
+				timer--;
+			if (timer == 90) 
+				Instantiate (text2, new Vector3 (0f, -2.27f, -1f), Quaternion.identity);
+			if (timer == 60)
+				Instantiate (text1, new Vector3 (0f, -2.27f, -1f), Quaternion.identity);
+			if (timer == 30)
+				Instantiate (textGo, new Vector3 (0f, 0f, -1f), Quaternion.identity);
+			if (timer <= 0)
+				gamestate = 1;
+		} else if (gamestate == 1) {
+			if (this.gameObject.transform.position.x >= 8.25)
+				gamestate = 2;
+			if (Input.GetKey (KeyCode.Space)) {
+				spaceDown = 1;
+			} else
+				spaceDown = -1;
+			rb.AddForce (new Vector2 (1, 5 * spaceDown));
+		} else if (gamestate == 2) {
+			Debug.Log ("You won");
+		} else if (gamestate == 2) {
+			Debug.Log ("You lost");
+		}
+
+	}
+
+
+
+	void OnCollisionEnter2D(Collision2D coll){
+		gamestate = 3;
+		Destroy(this.gameObject);
+		Instantiate (splosion, this.gameObject.transform.position, this.gameObject.transform.rotation);
+	}
+
+}
---
+++ Assets/text_decay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class text_decay : MonoBehaviour {
+
+	public int timer = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer--;
+		if(timer<=0) Destroy(this.gameObject);
+	}
+}
