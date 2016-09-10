d564999a9183937819a17aa7685d46a41014483f
Jequatot <jeffreyquattro@gmail.com>
2016-09-10 16:18:27 -0400

    Duck Defense / Mallard Menace

---
+++ Assets/Scripts/balance_platform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balance_platform : MonoBehaviour {
+
+    Rigidbody2D rb;
+    int direction = 1;
+    public int timer = 0;
+    // Use this for initialization
+    void Start () {
+            rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer++;
+        if(timer >= 575)
+        {
+            Application.LoadLevel("Game_Selector");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            direction *= -1;
+        }
+
+        rb.velocity = new Vector2(3f*direction, 0f);
+
+        if (direction ==-1 && this.transform.position.x <= -6.8)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+        if (direction == 1 && this.transform.position.x >= 6.8)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/duck_fall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duck_fall : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(rb.velocity.y < -5)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -5);
+        }
+	    if(this.transform.position.y < -4)
+        {
+            if (this.transform.position.x > -9 && this.transform.position.x < 9)
+            {
+                Application.LoadLevel("Game_Selector");
+            }
+            else Destroy(this.gameObject);
+        }
+	}
+}
