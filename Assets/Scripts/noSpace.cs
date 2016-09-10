0775b9186c86e6ac6a33e9cd5ef70ba057d24386
Jequatot <jeffreyquattro@gmail.com>
2016-09-10 14:51:51 -0400

    Don't Press Space

---
+++ Assets/Scripts/noSpace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class noSpace : MonoBehaviour {
+
+    public int timer = 120;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer--;
+	    if(timer <= 0)
+        {
+            Application.LoadLevel("Game_Selector");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("Game_Selector");
+        }
+	}
+}
