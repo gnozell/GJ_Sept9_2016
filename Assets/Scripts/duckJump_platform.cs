e2f8eced736b223316f97473a7753356fc65dd56
Jequatot <jeffreyquattro@gmail.com>
2016-09-11 15:42:32 -0400

    Duck Jump Game

---
+++ Assets/Scripts/duckJump_platform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class duckJump_platform : MonoBehaviour {
+
+    public float maxX;
+    public float minX;
+    int dir = 1;
+    Rigidbody2D rb;
+    
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(this.gameObject.transform.position.x > maxX)
+        {
+            dir = -1;
+        }
+        if (this.gameObject.transform.position.x < minX)
+        {
+            dir = 1;
+        }
+        rb.velocity = new Vector2(3f * dir, 0);
+    }
+}
---
+++ Assets/Scripts/duck_jump.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class duck_jump : MonoBehaviour {
+
+    Rigidbody2D rb;
+    //int velDecay = 250;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(rb.velocity.y > -.1 && rb.velocity.y < .1) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                rb.AddForce(new Vector2(0, 500));
+            }
+        }
+
+        if(this.transform.position.x >= 8) SceneManager.LoadScene("Game_Selector");
+        if(this.transform.position.y <= -6) SceneManager.LoadScene("Game_Selector_Fail");
+
+    }
+}
